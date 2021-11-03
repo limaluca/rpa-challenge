76fc5521debda7cc4698a0d00925f33fa806c22e
Lucas Lima <lucaslimamesmo@gmail.com>
2021-11-03 12:38:01 -0300

    Now the tracking number can be found and edited

--- tests.py
+++ tests.py
@@ -14,7 +14,7 @@ chrome_options = webdriver.ChromeOptions();
 chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']);
 
 driver = webdriver.Chrome('./chromedriver')
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(options=chroe_options)
 
 #Window configuration
 driver.get("https://marcelstein.com/elochallenge/starttest.php?token=ce3e75ea4b1681dec7ed81ab2e836599")
